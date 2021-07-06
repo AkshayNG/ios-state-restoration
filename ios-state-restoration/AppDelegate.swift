35406d7e26573d7e3c0cb2c25c88969008bca9e2
Akshay Gajarlawar <akshaygajarlawar@gmail.com>
2021-07-06 10:53:37 +0530

    Appdelegate: shouldRestoreSecureApplicationState

--- ios-state-restoration/AppDelegate.swift
+++ ios-state-restoration/AppDelegate.swift
@@ -10,7 +10,9 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    func application(_ application: UIApplication, shouldRestoreSecureApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
